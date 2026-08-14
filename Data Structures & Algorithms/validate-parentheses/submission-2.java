232966d3fb1982dae926a33fe1fe1ab82f7d2ffd
Daksh Purohit <dakshgrows@gmail.com>
2026-08-15 00:31:26 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                if(ch == ')' && stack.peek() == '(' || ch == '}' && stack.peek() == '{' || ch == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
